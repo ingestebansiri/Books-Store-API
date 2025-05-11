6998f46eb98627e0baff4495e4ba84aa1cd83246
ingestebansiri <89620094+ingestebansiri@users.noreply.github.com>
2025-05-11 17:03:50 -0300

    11-5

--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -1,16 +1,17 @@
 import { Router } from "express";
 import { getAllBooks, getByID, createBook, updateBook, deleteBook } from "../services/book.services.js";
+import { verifyToken } from "../services/auth.services.js";
 
 const router = Router();
 
-router.get("/books", getAllBooks);
+router.get("/books", verifyToken, getAllBooks);
 
-router.get("/books/:id", getByID);
+router.get("/books/:id", verifyToken, getByID);
 
-router.post("/books", createBook);
+router.post("/books", verifyToken, createBook);
 
-router.put("/books/:id", updateBook);
+router.put("/books/:id", verifyToken, updateBook);
 
-router.delete("/books/:id", deleteBook);
+router.delete("/books/:id", verifyToken, deleteBook);
 
 export default router;
--- src/services/auth.services.js
+++ src/services/auth.services.js
@@ -1,9 +1,47 @@
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken";
 import { User } from "../models/User.js";
+import { validateEmail, validatePassword, validateString } from "../helpers/validations.js";
 
+
+
+// Middleware para verificar el token JWT en las solicitudes
+export const verifyToken = (req, res, next) => {
+    // Obtiene el valor del header "Authorization" de la solicitud (si no existe, usa cadena vacía)
+    const header = req.header("Authorization") || "";
+
+    // Extrae el token desde el header
+    const token = header.split(" ")[1];
+
+    // Si no hay token, devuelve error 401 (no autorizado)
+    if (!token) {
+        return res.status(401).json({ message: "No posee autorización requerida" });
+    }
+
+    try {
+        // Verifica que el token sea válido usando la clave secreta
+        const payload = jwt.verify(token, 'programacion3-2025');
+
+        // Si el token es válido, se puede acceder a su contenido (payload)
+        console.log(payload);
+
+        // Llama al siguiente middleware o controlador en la cadena
+        next();
+    } catch (error) {
+        // Si el token no es válido o ha expirado, devuelve error 403 (prohibido)
+        return res.status(403).json({ message: "No posee permisos correctos" });
+    }
+}
 
 export const registerUser = async (req, res) => {
+
+    // Valida los datos recibidos con la función personalizada.
+    const result = validateRegisterUser(req.body);
+
+    if (result.error)
+        return res.status(400).send({ message: result.message })
+
+
     // Extrae name, email y password del body de la request
     const { name, email, password } = req.body;
 
@@ -37,6 +75,13 @@ export const registerUser = async (req, res) => {
 }
 
 export const loginUser = async (req, res) => {
+
+    // Valida los datos de la request con la función personalizada.
+    const result = validateLoginUser(req.body);
+
+    if (result.error)
+        return res.status(400).send({ message: result.message })
+
     // Extrae email y password del body de la request
     const { email, password } = req.body;
 
@@ -64,4 +109,57 @@ export const loginUser = async (req, res) => {
 
     // Devuelve el token al cliente
     return res.json(token);
+}
+
+const validateLoginUser = (req) => {
+    const result = {
+        error: false,
+        message: ''
+    }
+    const { email, password } = req;
+
+    if (!email || !validateEmail(email))
+        return {
+            error: true,
+            message: 'Mail inválido'
+        }
+
+    else if (!password || !validatePassword(password, 7, null, true, true)) {
+        return {
+            error: true,
+            message: 'Contraseña inválida'
+        }
+    }
+
+    return result;
+}
+
+const validateRegisterUser = (req) => {
+    const result = {
+        error: false,
+        message: ''
+    }
+
+    const { name, email, password } = req;
+
+    if (!name || !validateString(name, null, 13))
+        return {
+            error: true,
+            message: 'Nombre de usuario inválido'
+        }
+
+    if (!email || !validateEmail(email))
+        return {
+            error: true,
+            message: 'Mail inválido'
+        }
+
+    else if (!password || !validatePassword(password, 7, null, true, true)) {
+        return {
+            error: true,
+            message: 'Contraseña inválida'
+        }
+    }
+
+    return result;
 }
